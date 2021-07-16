28cfa44d93dd81b1935643e5217b6b3df7d98009
PercevalNSC <wait-keita1998@outlook.jp>
2021-07-16 21:42:04 +0900

    nekoneko

--- serverGLH/CreateList.py
+++ serverGLH/CreateList.py
@@ -1,38 +1,51 @@
-class DocumentLngLatList :
-    # 1つのドキュメントについて緯度経度のリストを返す
-    def __init__(self, document):
-        self.document = document
-    
-    def docAsSrpList(self):
-        return self.makingList("activitySegment" , "simplifiedRawPath", "points")
-    def docAsWpPList(self):
-        return self.makingList("activitySegment", "waypointPath", "waypoints")
-    def docPvSrpList(self):
-        return self.makingList("placeVisit", "simplifiedRawPath", "points")
-    
-    def makingList(self,segment, value1, value2):
-        lnglatlist = []
-        for item in self.document[segment][value1][value2] :
-            lnglatlist.append([item["lngE7"], item["latE7"]])
-        return lnglatlist
+class BaseLngLatList :
+    def __init__(self, corsor):
+        self.corsor = corsor
+        self.segment = ""
+        self.v1 = ""
+        self.v2 = ""
+        self.lnglatlist = []
+    def makingDocumentList(self, document):
+        documentlist = []
+        for item in document[self.segment][self.v1][self.v2] :
+            documentlist.append([item["lngE7"], item["latE7"]])
+        return documentlist
+    def makingFieldList(self):
+        for document in self.corsor :
+            doclist = self.makingDocumentList(document)
+            for item in doclist:
+                self.lnglatlist.append(item)
 
-class MakingLngLatList :
-    # １つのコレクションに対し、ドキュメントの緯度経度リストから緯度経度リストを作成する
-    def __init__(self, collection):
-        self.collection = collection
-    def clctAsSrpList(self):
-        lnglatlists = []
-        for document in self.collection:
-            dllObj = DocumentLngLatList(document)
-            dll = dllObj.docAsSrpList()
-            for i in dll:
-                lnglatlists.append(i)
-        return lnglatlists
+class AsSimplifiedRawPath(BaseLngLatList):
+    def __init__(self, corsor):
+        super().__init__(corsor)
+        self.segment = "activitySegment"
+        self.v1 = "simplifiedRawPath"
+        self.v2 = "points"
+
+class AsWaypointPath(BaseLngLatList):
+    def __init__(self, corsor):
+        super().__init__(corsor)
+        self.segment = "activitySegment"
+        self.v1 = "waypointPath"
+        self.v2 = "waypoints"
+
+class PvSimplifiedRawPath(BaseLngLatList):
+    def __init__(self, corsor):
+        super().__init__(corsor)
+        self.segment = "placeVisit"
+        self.v1 = "simplifiedRawPath"
+        self.v2 = "points"
+    
 
 if __name__ == "__main__" :
-    test_item = {'_id': '60e1141eeb127c56412ef5b5', 'activitySegment': {'simplifiedRawPath': {'points': [{'latE7': 35.6657028, 'lngE7': 139.5432739, 'timestampMs': 1525071569400, 'accuracyMeters': 20}, {'latE7': 35.6597595, 'lngE7': 139.5408325, 'timestampMs': 1525071815400, 'accuracyMeters': 11}, {'latE7': 35.6570778, 'lngE7': 139.5430298, 'timestampMs': 1525071940700, 'accuracyMeters': 22}]}}}
+    from pymongo import MongoClient
 
-    test_list = DocumentLngLatList(test_item)
-    list = test_list.docAsSrpList()
+    with MongoClient("mongodb://127.0.0.1:27017") as client:
+        test_db = client.glh_db
+        test_collection = test_db.glh_clct_1
+        corsor = test_collection.find({"activitySegment.simplifiedRawPath": {"$exists": True}},{"activitySegment.simplifiedRawPath": 1})
 
-    print(test_list)
+    obj = AsSimplifiedRawPath(corsor)
+    obj.makingFieldList()
+    print(obj.lnglatlist)
--- serverGLH/flask_test.py
+++ serverGLH/flask_test.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, make_response
 from pymongo import MongoClient
-from CreateList import MakingLngLatList
+from CreateList import AsSimplifiedRawPath
 import json
 
 app = Flask(__name__)
@@ -9,18 +9,21 @@ app = Flask(__name__)
 def home():
     return render_template('index2.html')
 
-@app.route('/returnjson')
-def returnjson():
+@app.route('/api/<path>')
+def serveapi(path):
+    path = path
+
     with MongoClient("mongodb://127.0.0.1:27017") as client:
         test_db = client.glh_db
         test_collection = test_db.glh_clct_2
-        corsor = test_collection.find({"activitySegment.simplifiedRawPath": {"$exists": True}},{"activitySegment.simplifiedRawPath": 1})
+        corsor = test_collection.find({path: {"$exists": True}},{path: 1})
 
-    listObj = MakingLngLatList(corsor)
-    resultdata = {"dataType": "AcitivitySegment.simplifiedRawPath", "data": listObj.clctAsSrpList()}
+    listObj = AsSimplifiedRawPath(corsor)
+    listObj.makingFieldList()
+    resultdata = {"dataType": path, "data": listObj.lnglatlist}
 
-    with open("dbload.json", "w") as f :
-        json.dump(resultdata, f, indent=2,ensure_ascii=False)
+    #with open("dbload.json", "w") as f :
+        #json.dump(resultdata, f, indent=2,ensure_ascii=False)
     return make_response(json.dumps(resultdata, indent=2, ensure_ascii=False))
 
 @app.route('/geocoder')
@@ -28,5 +31,9 @@ def geocoderMap():
     title = "Flask app"
     framework = "Flask"
     return render_template('index.html', title=title, framework=framework)
+
+@app.route('/<name>')
+def test(name):
+    return name
 
 app.run(port=8000, debug=True)
