25c89003fe61161cd97beecc0f74a8c9e0c9c4b9
PercevalNSC <wait-keita1998@outlook.jp>
2021-09-04 11:41:49 +0900

    add placeVisi data, and output png in dataTrend

--- storeGLH/dataTrend.py
+++ storeGLH/dataTrend.py
@@ -1,10 +1,77 @@
+from platform import dist
 from pymongo import MongoClient
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__' :
+
+def listDifference(points):
+    assert len(points) > 0, "points is None"
+    return list(map((lambda x: x - points[0]), points))[1:]
+
+def pointsDifference(points):
+    result = points
+    indexes = ["latE7", "lngE7", "timestampMs"]
+
+    for index in indexes:
+        diff = result[0][index]
+        for p in result:
+            p[index] = p[index] - diff
+     
+    return result[1:]
+
+def differenceList(segment):
+    result = []
+
+    query = { segment + ".simplifiedRawPath" : {"$exists": True}}
+    for doc in queryMongodb(query):
+        points = doc[segment]["simplifiedRawPath"]["points"]
+        diff = pointsDifference(points)
+        result.extend(diff)
+
+    return result
+
+def docDistance(doc):
+    return (doc["latE7"]**2 + doc["lngE7"]**2)
+
+
+def queryMongodb(query):
     with MongoClient("mongodb://127.0.0.1:27017") as client:
         glh_db = client.glh_db
         glh_clct = glh_db.glh_clct_2
-        corsor = glh_clct.find()
+        return glh_clct.find(query)
+    
+def createFigure(distlists,timelists):
+    fig = plt.figure()
 
-    for i in corsor :
-        print(i)
+    # full | As
+    # full | Pv
+    fullax = fig.add_subplot(1, 2, 1, title = "Full", xlabel = "distance", ylabel = "time")
+    fullax.scatter(distlists[0] + distlists[1], timelists[0] + timelists[1], marker=".")
+    asax = fig.add_subplot(2, 2, 2, title = "ActivitySegment")
+    asax.scatter(distlists[0], timelists[0], marker=".")
+    pvax = fig.add_subplot(2, 2, 4, title = "PlaceVisit")
+    pvax.scatter(distlists[1],timelists[1], marker=".")
+    
+    fig.tight_layout()
+    fig.savefig("difference.png")
+ 
+if __name__ == '__main__' :
+
+    segment = ["activitySegment", "placeVisit"]
+    distlists = []
+    timelists = []
+
+    for seg in segment:
+        distlist = []
+        timelist = []
+        difflist = differenceList(seg)
+
+        for doc in difflist :
+            distlist.append(docDistance(doc))
+            timelist.append(doc["timestampMs"])
+
+        # print([docDistance(doc) for doc in difflist])
+        
+        distlists.append(distlist)
+        timelists.append(timelist)
+
+    createFigure(distlists, timelists)
--- storeGLH/storeGLH.py
+++ storeGLH/storeGLH.py
@@ -1,7 +1,8 @@
 import json
+import os
+import glob
 import GLHload
 from pymongo import MongoClient
-import glob
 
 # fileを開いて初期化したオブジェクトを返す
 def jsonOpen(filename):
@@ -12,17 +13,19 @@ def jsonOpen(filename):
     glhdata.glhinit()
     return glhdata
 
-def insertDict(filelist):
+# ファイル名のリストからファイルを開いてMongoDBへ格納
+def insertGLH(filelist):
+
     with MongoClient("mongodb://127.0.0.1:27017") as client:
         glh_db = client.glh_db
 
-    glh_clct = glh_db.glh_clct_full
-    glh_clct.delete_many({})
+        glh_clct = glh_db.glh_clct_full
+        glh_clct.delete_many({})
 
-    for filename in filelist :
-        glhdata = jsonOpen(filename)
-        glh_clct.insert_many(glhdata.dict["timelineObjects"])
-        print("loaded: " + filename)
+        for filename in filelist :
+            glhdata = jsonOpen(filename)
+            glh_clct.insert_many(glhdata.dict["timelineObjects"])
+            print("loaded: " + filename)
 
 # rootpath以下の月別GLHjsonファイル名のリストを返す
 def getjsondata(rootpath):
@@ -32,7 +35,7 @@ def getjsondata(rootpath):
     for y in years:
         path = rootpath + str(y) + "/*.json"
         filelist.extend(glob.glob(path))
-    
+
     return filelist
 
 if __name__ == '__main__' :
@@ -40,8 +43,8 @@ if __name__ == '__main__' :
     rootpath = "./jsondata/"
 
     filelist = getjsondata(rootpath)
-    # print(filelist)
-    insertDict(filelist)
+    assert len(filelist) > 0, "filelist is None. Check carrent directory: " + os.getcwd()
+    insertGLH(filelist)
 
     # mongodb command
         # sudo service mongodb start/stop/status
