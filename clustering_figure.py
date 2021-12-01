f6c9e43539488ea7a060e4156fb60fcaf9e3a5ca
PercevalNSC <wait-keita1998@outlook.jp>
2021-12-01 10:31:45 +0900

    neko

---
+++ clustering_figure.py
@@ -0,0 +1,15 @@
+from GLHMongoDB import OPTICSConstruct
+from GLH.GLHmodule.Plotfigure import cluster_figure
+from GLH.GLHmodule.Clustering import LabeledTrajectoryData
+
+eps = 0.0001
+min_samples = 4
+obj = OPTICSConstruct(min_samples)
+obj.set_eps(eps)
+obj.clustering_obj.clustering.reachability_plot()
+labeled_trajectory_list = obj.labeled_trajectory_data()
+ltd = LabeledTrajectoryData(labeled_trajectory_list)
+label_term = ltd.cluster_term()
+print(label_term)
+cluster_figure(label_term, "cluster_time", "time")
+
---
+++ ground.py
@@ -0,0 +1,12 @@
+import GLH.GLHmodule.Clustering as gc
+import math
+
+a1 = [36.1037, 140.0878]
+a2 = [35.6550, 139.7447]
+dist = math.sqrt((a1[0] - a2[0])**2 +  (a1[1] - a2[1])**2)
+real_dist = 58.643
+print(dist)
+print(real_dist)
+print(gc.geography_to_euclid(real_dist))
+print(gc.euclid_to_geography(dist))
+print(gc.geography_to_euclid(2))
